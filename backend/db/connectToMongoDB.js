4c36e35cc0935586efc461162a05264a40f7fd6e
Vaibhav Pandey <97404306+RUDDY29@users.noreply.github.com>
2024-05-28 21:41:11 +0530

    Update connectToMongoDB.js

--- backend/db/connectToMongoDB.js
+++ backend/db/connectToMongoDB.js
@@ -4,7 +4,7 @@ dotenv.config();
 
 const connectToMongoDB = async () => {
 	try {
-		await mongoose.connect();
+		await mongoose.connect('mongodb+srv://vibhu7838:vaibhav@cluster0.qbhdrby.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0');
 		console.log("Connected to MongoDB");
 	} catch (error) {
 		console.log("Error connecting to MongoDB", error.message);
